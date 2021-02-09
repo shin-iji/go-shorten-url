8d2c0c3969446390ab8c0a802177e0cc9e4699c3
bEERy <siradanai27@gmail.com>
2021-02-09 11:58:50 +0700

    Updated Messages

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World! 123")
+		return c.String(http.StatusOK, "Hello, World! hahaha")
 	})
 	e.Logger.Fatal(e.Start(":8000"))
 }
