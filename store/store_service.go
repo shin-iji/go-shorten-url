0caa881bfe898716502734c7bc9f54031b06a858
bEERy <siradanai27@gmail.com>
2021-03-06 16:34:45 +0700

    Added redis

--- store/store_service.go
+++ store/store_service.go
@@ -1,16 +1,47 @@
 package store
 
 import (
+	"context"
 	"fmt"
+	"time"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/shin-iji/go-shorten-url/database"
 
 	_ "github.com/lib/pq"
 )
 
+var (
+	storeService = &StorageService{}
+	ctx          = context.Background()
+	db           = database.OpenConnection()
+)
+
+const CacheDuration = 6 * time.Hour
+
+type StorageService struct {
+	redisClient *redis.Client
+}
+
+func InitializeStore() *StorageService {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Error init Redis: %v", err))
+	}
+
+	fmt.Printf("\nRedis started successfully: pong message = {%s}", pong)
+	storeService.redisClient = redisClient
+	return storeService
+}
+
 func SaveURLMapping(shortURL string, originalURL string) {
-	db := database.OpenConnection()
-	defer db.Close()
+	//defer db.Close()
 	sqlStatement := `INSERT INTO Shorten_URL (shortURL, originalURL, count) VALUES ($1, $2, 0)`
 	_, err := db.Query(sqlStatement, shortURL, originalURL)
 	if err != nil {
@@ -20,16 +51,22 @@ func SaveURLMapping(shortURL string, originalURL string) {
 }
 
 func RetrieveInitialURL(shortURL string) string {
+	//defer db.Close()
 	var result string
-	db := database.OpenConnection()
-	defer db.Close()
-	sqlStatement := `SELECT originalURL FROM Shorten_URL WHERE shorturl = $1;`
-	row := db.QueryRow(sqlStatement, shortURL)
-	err := row.Scan(&result)
+	result, err := storeService.redisClient.Get(ctx, shortURL).Result()
 	if err != nil {
-		panic(fmt.Sprintf("Failed RetrieveInitialUrl url | Error: %v - shortUrl: %s\n", err, shortURL))
+		sqlStatement := `SELECT originalURL FROM Shorten_URL WHERE shorturl = $1;`
+		row := db.QueryRow(sqlStatement, shortURL)
+		err = row.Scan(&result)
+		if err != nil {
+			panic(fmt.Sprintf("Failed RetrieveInitialUrl url | Error: %v - shortUrl: %s\n", err, shortURL))
+		}
+		err := storeService.redisClient.Set(ctx, shortURL, result, CacheDuration).Err()
+		if err != nil {
+			panic(fmt.Sprintf("Failed saving key url | Error: %v - shortUrl: %s - originalUrl: %s\n", err, shortURL, result))
+		}
 	}
-	sqlStatement = `UPDATE Shorten_URL SET count = count + 1 WHERE shorturl = $1;`
+	sqlStatement := `UPDATE Shorten_URL SET count = count + 1 WHERE shorturl = $1;`
 	_, err = db.Query(sqlStatement, shortURL)
 	if err != nil {
 		panic(fmt.Sprintf("Failed IncreaseViewPage url | Error: %v - shortUrl: %s\n", err, shortURL))
@@ -38,14 +75,13 @@ func RetrieveInitialURL(shortURL string) string {
 }
 
 func GetLinkCount(shortURL string) int {
+	//defer db.Close()
 	var count int
-	db := database.OpenConnection()
 	sqlStatement := `SELECT count FROM Shorten_URL WHERE shorturl = $1;`
 	row := db.QueryRow(sqlStatement, shortURL)
 	err := row.Scan(&count)
 	if err != nil {
 		panic(fmt.Sprintf("Failed GetLinkCount url | Error: %v - shortUrl: %s\n", err, shortURL))
 	}
-	defer db.Close()
 	return count
 }
